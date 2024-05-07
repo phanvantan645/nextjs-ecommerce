d7f39f8261bfb5939e7b2278413974f5ed2d035e
Van Tan <phanvantan645@gmail.com>
2024-05-07 14:37:14 +0700

    add shadcnUI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from 'next/image';
+import { Button } from '~/components/ui/button';
 
-export default function Home() {
-    return <h1>hello</h1>;
+export default function Page() {
+    return (
+        <h1>
+            Hello, Next.js!
+            <div>
+                <Button>Hello</Button>
+            </div>
+        </h1>
+    );
 }
